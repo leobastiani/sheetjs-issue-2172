0839721f544eefab64caf4b2e4a9cea411895ab6
Leonardo Guarnieri de Bastiani <leogbastiani@gmail.com>
2020-11-10 19:01:14 -0300

    Feedback applied

--- Java/src/main/java/Generator.java
+++ Java/src/main/java/Generator.java
@@ -9,10 +9,15 @@ public class Generator {
         Workbook wb = new Workbook();
         Worksheet ws = wb.getWorksheets().get(0);
         ws.setName("Example");
-        ws.getCells().get(0, 0).setValue("A1");
-        ws.getCells().get(0, 1).setValue("B1");
-        ws.getCells().get(1, 0).setValue("A2");
-        ws.getCells().get(2, 2).setValue("C3");
+        ws.getCells().get(0, 0).setValue(true);
+        ws.getCells().get(0, 1).setValue(true);
+        ws.getCells().get(0, 2).setValue(false);
+        ws.getCells().get(1, 0).setValue(true);
+        ws.getCells().get(1, 1).setFormula("=#NUM!");
+        ws.getCells().get(1, 2).setFormula("=#VALUE!");
+        ws.getCells().get(2, 0).setFormula("=#NUM!");
+        ws.getCells().get(2, 1).setValue(true);
+        ws.getCells().get(2, 2).setValue(false);
         Path path = Paths.get("..", "wb.xlsb").normalize().toAbsolutePath();
         wb.save(path.toString());
     }
