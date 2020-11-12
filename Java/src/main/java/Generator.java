94cace7eb03d4fc3555f435a53a08f7e18d878cb
Leonardo Guarnieri de Bastiani <leogbastiani@gmail.com>
2020-11-12 11:13:06 -0300

    Added setExportAllColumnIndexes

--- Java/src/main/java/Generator.java
+++ Java/src/main/java/Generator.java
@@ -1,5 +1,6 @@
 import com.aspose.cells.Workbook;
 import com.aspose.cells.Worksheet;
+import com.aspose.cells.XlsbSaveOptions;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -9,11 +10,22 @@ public class Generator {
         Workbook wb = new Workbook();
         Worksheet ws = wb.getWorksheets().get(0);
         ws.setName("Example");
-        ws.getCells().get(0, 0).setValue("A1");
-        ws.getCells().get(0, 1).setValue("B1");
-        ws.getCells().get(1, 0).setValue("A2");
-        ws.getCells().get(2, 2).setValue("C3");
-        Path path = Paths.get("..", "wb.xlsb").normalize().toAbsolutePath();
-        wb.save(path.toString());
+        ws.getCells().get(0, 0).setValue(true);
+        ws.getCells().get(0, 1).setValue(true);
+        ws.getCells().get(0, 2).setValue(false);
+        ws.getCells().get(1, 0).setValue(true);
+        ws.getCells().get(1, 1).setValue("#NUM!");
+        ws.getCells().get(1, 2).setValue("#VALUE!");
+        ws.getCells().get(2, 0).setValue("#NUM!");
+        ws.getCells().get(2, 1).setValue(true);
+        ws.getCells().get(2, 2).setValue(false);
+        XlsbSaveOptions saveOptions = new XlsbSaveOptions();
+        String envVar = System.getenv("EXPORT_ALL_COLUMN_INDEXES");
+        boolean safe = "1".equals(envVar);
+        System.out.print("Safe: ");
+        System.out.println(safe);
+        saveOptions.setExportAllColumnIndexes(safe);
+        Path path = Paths.get("..", "wb-" + (safe ? "" : "un") + "safe.xlsb").normalize().toAbsolutePath();
+        wb.save(path.toString(), saveOptions);
     }
 }
--- README.md
+++ README.md
@@ -5,6 +5,10 @@ https://github.com/SheetJS/sheetjs/issues/2172
 # Steps
 
 ```bash
-(cd Java && mvn compile package exec:java)
+(cd Java && EXPORT_ALL_COLUMN_INDEXES="0" mvn compile package exec:java)
+(cd Java && EXPORT_ALL_COLUMN_INDEXES="1" mvn compile package exec:java)
+cp -f wb-safe.xlsb wb.xlsb
+(cd Node && yarn install && node index.js)
+cp -f wb-unsafe.xlsb wb.xlsb
 (cd Node && yarn install && node index.js)
 ```
